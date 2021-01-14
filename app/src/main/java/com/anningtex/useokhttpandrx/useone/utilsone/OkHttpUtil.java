6013c4b948577589a012630d1e03ae2adfec56c0
Administrator <1020492220@qq.com>
2021-01-14 15:47:07 +0800

    增加同步请求在OkHttpUtil文件里

--- app/src/main/java/com/anningtex/useokhttpandrx/useone/utilsone/OkHttpUtil.java
+++ app/src/main/java/com/anningtex/useokhttpandrx/useone/utilsone/OkHttpUtil.java
@@ -1,9 +1,13 @@
 package com.anningtex.useokhttpandrx.useone.utilsone;
 
+import java.io.IOException;
+
+import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * @Author Song
@@ -11,12 +15,63 @@ import okhttp3.RequestBody;
  */
 public class OkHttpUtil {
 
+    /**
+     * 异步
+     */
     public static void sendOkHttpRequest(String address, RequestBody requestBody, Callback callback) {
         OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
+        Request.Builder builder = new Request.Builder()
                 .url(address)
-                .post(requestBody)
-                .build();
+                .post(requestBody);
+//        if (MainApplication.SESSION != null && !MainApplication.SESSION.isEmpty()) {
+//            builder.addHeader("Cookie", MainApplication.SESSION);
+//        }
+        Request request = builder.build();
         client.newCall(request).enqueue(callback);
     }
+
+    /**
+     * 同步
+     */
+    private static Response response;
+
+    public static Response sendOkHttpRequestSynchronize(String address, RequestBody requestBody) {
+        OkHttpClient client = new OkHttpClient();
+        Request.Builder builder = new Request.Builder()
+                .url(address)
+                .post(requestBody);
+//        if (MainApplication.SESSION != null && !MainApplication.SESSION.isEmpty()) {
+//            builder.addHeader("Cookie", MainApplication.SESSION);
+//        }
+        Request request = builder.build();
+        Call call = client.newCall(request);
+        try {
+            response = call.execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return response;
+    }
+
+    //同步的调用示例
+//    private void eg() {
+//        new Thread(() -> {
+//            FormBody.Builder requestBuild = new FormBody.Builder();
+//            RequestBody requestBody = requestBuild
+//                    .add("key", "value")
+//                    .build();
+//            Response response = OkHttpUtil.sendOkHttpRequestSynchronize(URL, requestBody);
+//            try {
+//                if (response.body() != null) {
+//                    String result = response.body().string();
+//                    Log.e("result_Success : ", result);
+//                    //start write
+//                }
+//            } catch (
+//                    IOException e) {
+//                e.printStackTrace();
+//            }
+//        }).start();
+//    }
 }
